324893a824e25027e75b5f7eeae41a5730f4de80
Yago Calixto <yagocalixto020@gmail.com>
2025-12-30 17:23:06 -0300

    refactor(core): translate table tennis simulation to English
    Renamed functions, variables, comments, and docstrings from Portuguese
    to English for clarity and consistency. Corrected typos in variable
    names and improved docstrings.
    
    BREAKING CHANGE: All variable names and comments are now in English,
    which may require updates in dependent scripts or tests.

--- atletas.py
+++ atletas.py
@@ -1,27 +1,28 @@
-class Atleta:
-    def __init__(self, nome, ataque, defesa, saque):
-        self.nome = nome
-        self.ataque = ataque
-        self.defesa = defesa
-        self.saque = saque
+class Athlete:
+    def __init__(self, name, attack, defense, serve):
+        self.name = name
+        self.attack = attack
+        self.defense = defense
+        self.serve = serve
     
     def __str__(self):
         return f'''
-Atleta: {self.nome}
-Ataque: {self.ataque}
-Defesa: {self.defesa}'''
+Athlete: {self.name}
+attack: {self.attack}
+defense: {self.defense}
+serve: {self.serve}'''
 
 
-calderano = Atleta('Hugo Calderano', 18, 17, 7)
-truls = Atleta('Truls Möregårdh', 16, 15, 8)
-bruna_takahashi = Atleta('Bruna Takahashi', 14, 16, 7)
-giulia_takahashi = Atleta('Giulia Takahashi', 13, 15, 16)
-felix_lebrun = Atleta('Félix Lebrun', 19, 15, 9)
-chuqin = Atleta('Wang Chuqin', 19, 18, 9)
+calderano = Athlete('Hugo Calderano', 18, 17, 7)
+truls = Athlete('Truls Möregårdh', 16, 15, 8)
+bruna_takahashi = Athlete('Bruna Takahashi', 14, 16, 7)
+giulia_takahashi = Athlete('Giulia Takahashi', 13, 15, 16)
+felix_lebrun = Athlete('Félix Lebrun', 19, 15, 9)
+chuqin = Athlete('Wang Chuqin', 19, 18, 9)
 
-atletas = [
+athletes_list = [
     calderano, truls, 
     bruna_takahashi, giulia_takahashi,
     felix_lebrun, chuqin
 ]
-# backspin, topspin, forehand, backhand, serve, ataque, defesa, double (duas maos)
+# backspin, topspin, forehand, backhand, serve, attack, defense, double (duas maos)
