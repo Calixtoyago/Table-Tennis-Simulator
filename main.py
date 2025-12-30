ef6e5b87104d445c035ccd2d2dd12177bdf0fe93
Yago Calixto <yagocalixto020@gmail.com>
2025-12-30 15:49:49 -0300

    feat: real time simulation

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-import random
-from atletas import atletas
+import random, time, pandas as pd, os
+from atletas import Atleta, atletas
+from tabulate import tabulate
 
+def limpar_tela(): # o nome é auto explicativo
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def mostrar_resultado(games=dict, j1=Atleta, j2=Atleta):
+    limpar_tela()
+
+    placar = {
+        j1.nome: [],
+        j2.nome: []
+    }
+    columns = ['Sets']
+    for game in games.items():
+        sets, pontos = game
+        placar[j1.nome].append(pontos[0])
+        placar[j2.nome].append(pontos[1])
+        columns.append(str(sets))
+
+    df_placar = pd.DataFrame.from_dict(placar, orient='index')
+    placar = tabulate(df_placar, headers=columns, stralign='center', numalign='right', tablefmt='rounded_outline')
+    print(placar)
+
+'''
+funcao para alternar o sacador do ponto
+no tenis de mesa o saque é alternado a cada 2 pontos independente de quem marcou
+unica mudanca é quando a partida chega em Deuce, que é quando fica 10x10 e o primeiro que marcar 2 pontos consecutivos ganha o set
+durante o Deuce, o saque alterna a cada ponto, também independente de quem marcou o ponto
+'''
 def alternar_saque(deuce, cont_saque, jogadores, sacar, receber):
     if deuce:
         sacar, receber = (jogadores[1], jogadores[0]) if sacar == jogadores[0] else (jogadores[0], jogadores[1])
@@ -11,7 +39,7 @@ def alternar_saque(deuce, cont_saque, jogadores, sacar, receber):
 
     return sacar, receber, cont_saque
 
-def Partida(j1, j2):
+def partida(j1, j2):
     games = {}
 
     game_j1, game_j2 = 0, 0
@@ -33,8 +61,10 @@ def Partida(j1, j2):
     while game_j1 < 3 and game_j2 < 3:
         deuce = False
         while True:
-            
-            print(f'Saque: {sacar.nome}')
+            games[game] = (pontos_j1, pontos_j2)
+            time.sleep(1)
+            mostrar_resultado(games, j1, j2)
+            # print(f'Saque: {sacar.nome}')
 
             chance_erro = (10 - sacar.saque) / 10
             chance_ace = (sacar.saque / (sacar.saque + receber.defesa)) * 0.15
@@ -44,13 +74,13 @@ def Partida(j1, j2):
                     pontos_j2 += 1
                 else:
                     pontos_j1 += 1
-                print('Errou o saque')
+                # print('Errou o saque')
             elif random.random() < chance_ace: # fez um ace
                 if sacar == j1:
                     pontos_j1 += 1
                 else:
                     pontos_j2 += 1
-                print('Fez Ace')
+                # print('Fez Ace')
             else: # continua o jogo
                 random.shuffle(jogadores) #aleatorizar o jogador a atacar
 
@@ -74,7 +104,6 @@ def Partida(j1, j2):
                 if pontos_j1 == 10 and pontos_j2 == 10:
                     deuce = True # quando os sets passam a ser vencidos com a diferenca de 2
                     # vai ser usado para mudar a alternancia dos saques para 1 ponto
-                    print('-- DEUCE --')
 
             cont_saque += 1 #contador dos pontos para alternancia dos saques
 
@@ -87,7 +116,6 @@ def Partida(j1, j2):
             if pontos_j2 >= 11 and pontos_j2 - pontos_j1 >= 2:
                 game_j2 += 1
                 break
-        print('Fim do game')
 
         games[game] = (pontos_j1, pontos_j2)
         pontos_j1 = 0
@@ -98,10 +126,8 @@ def Partida(j1, j2):
         print(f'{j1.nome} GANHOU')
     else:
         print(f'{j2.nome} GANHOU')
-
-    for games_finished, sets in games.items():
-        print(f"{games_finished}: {sets}")
+    mostrar_resultado(games, j1, j2)
 
     return j1.nome if game_j1 > game_j2 else j2.nome
 
-Partida(atletas[0], atletas[1])
+partida(atletas[0], atletas[1])
