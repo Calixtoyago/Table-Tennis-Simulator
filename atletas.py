303817af78a776ff8045c5d67e9c23b7e7139354
Yago Calixto <yagocalixto020@gmail.com>
2025-12-30 16:27:35 -0300

    feat(atletas.py): add new athletes
    Bruna Takahashi, Giulia Takahashi, Felix Lebrun, Wang Chuqin

--- atletas.py
+++ atletas.py
@@ -12,8 +12,16 @@ Ataque: {self.ataque}
 Defesa: {self.defesa}'''
 
 
-calderano = Atleta('Hugo Calderano', 15, 14, 7)
-truls = Atleta('Truls Möregårdh', 14, 13, 7)
+calderano = Atleta('Hugo Calderano', 18, 17, 7)
+truls = Atleta('Truls Möregårdh', 16, 15, 8)
+bruna_takahashi = Atleta('Bruna Takahashi', 14, 16, 7)
+giulia_takahashi = Atleta('Giulia Takahashi', 13, 15, 16)
+felix_lebrun = Atleta('Félix Lebrun', 19, 15, 9)
+chuqin = Atleta('Wang Chuqin', 19, 18, 9)
 
-atletas = [calderano, truls]
+atletas = [
+    calderano, truls, 
+    bruna_takahashi, giulia_takahashi,
+    felix_lebrun, chuqin
+]
 # backspin, topspin, forehand, backhand, serve, ataque, defesa, double (duas maos)
